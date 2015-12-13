dd8d75192d188389c8bdba11f9ce195e5b42f138
June Cong <junzhucong@gmail.com>
2015-12-13 02:47:59 -0800

    commit after I fucked up

---
+++ Pascal/Vision/CMakeFiles/FSM.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FSM.dir/FSM.cpp.o"
+  "libFSM.pdb"
+  "libFSM.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/FSM.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
